80a63232c32b17a66602629a28c803f72d0b4849
Yego <eddieyego273@gmail.com>
2025-03-14 13:01:34 +0300

    incomplete

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(activity=roller-skate){
+    return `This Saturday, I want to ${activity}`
+}
+const fn = function() {
+    return "Yet more razzling.";
+};
+
+const mondayWork = function(st = "go to the office") {
+    return `This Monday, I will ${st}.`;
+};
+
+function wrapAdjective(wrap = "*") {
+    return function(adj = "special") {
+        return `You are ${wrap}${adj}${wrap}!`;
+    }
+}
